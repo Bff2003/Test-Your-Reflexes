08f19fc6028aee45217940785cefd6e7936da7bf
bff2003 <bernardofarrobinha1@gmail.com>
2024-12-23 14:05:27 +0000

    Adding pose detector

---
+++ src/detectors/PoseDetector.py
@@ -0,0 +1,109 @@
+import pprint
+import numpy as np
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+from mediapipe.framework.formats import landmark_pb2
+from mediapipe import solutions
+from mediapipe.python.solutions import drawing_styles
+import cv2
+
+class PoseDetector:
+    def __init__(self):
+        # Initialize the object detector
+        base_options = python.BaseOptions(model_asset_path='models/poses/pose_landmarker_heavy.task')
+        options = vision.PoseLandmarkerOptions(base_options=base_options, output_segmentation_masks=True)
+        self.detector = vision.PoseLandmarker.create_from_options(options)
+
+    def visualize(self, image, detections: list):
+        pose_landmarks_list = detections
+        annotated_image = np.copy(image)
+
+        # Loop through the detected poses to visualize.
+        for idx in range(len(pose_landmarks_list)):
+            pose_landmarks = pose_landmarks_list[idx]
+
+            # Draw the pose landmarks.
+            pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+            pose_landmarks_proto.landmark.extend([
+            landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
+            ])
+            solutions.drawing_utils.draw_landmarks(
+            annotated_image,
+            pose_landmarks_proto,
+            solutions.pose.POSE_CONNECTIONS,
+            solutions.drawing_styles.get_default_pose_landmarks_style())
+        return annotated_image
+    
+    def detect(self, frame) -> list:
+        """ Detect objects in the frame and return a list of detections. """
+        # Convert the image to RGB.
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        # Process the frame.
+        image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
+
+        # Detect objects in the frame.
+        detections = self.detector.detect(image)
+
+        # for detection in detections.pose_landmarks:
+        #     print(detection)
+        return detections.pose_landmarks
+
+
+    def run(self):
+        # Create a video capture object to read frames from the camera.
+        cap = cv2.VideoCapture(0)
+
+        # Process each frame.
+        while cap.isOpened():
+            success, image = cap.read()
+            if not success:
+                print("Ignoring empty camera frame.")
+                continue
+
+            # Convert the image to RGB.
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+            # Process the frame.
+            image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
+
+            # Detect objects in the frame.
+            detection_result = self.detector.detect(image)
+
+            # Visualize the detection result.
+            image_copy = np.copy(image.numpy_view())
+            annotated_image = self.visualize(image_copy, detection_result)
+
+            # Display the annotated frame.
+            cv2.imshow('MediaPipe Object Detection', annotated_image)
+            if cv2.waitKey(5) & 0xFF == 27:
+                break
+
+        # Release resources.
+        cap.release()
+
+if __name__ == "__main__":
+    detector = PoseDetector()
+    
+    # Create a video capture object to read frames from the camera.
+    cap = cv2.VideoCapture(0)
+
+    # Process each frame.
+    while cap.isOpened():
+        success, image = cap.read()
+        if not success:
+            print("Ignoring empty camera frame.")
+            continue
+
+        detections = detector.detect(image)
+        image = detector.visualize(image, detections)
+
+        # Display the annotated frame.
+        cv2.imshow('MediaPipe Object Detection', image)
+
+        if cv2.waitKey(5) & 0xFF == 27:  
+            break
+    
+    # Release resources.
+    cap.release()
