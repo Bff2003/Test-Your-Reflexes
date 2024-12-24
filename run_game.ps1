4170715883dd95c77262b1811a14f1737df95070
bff2003 <bernardofarrobinha1@gmail.com>
2024-12-24 14:12:28 +0000

    Adding script to run the game

---
+++ run_game.ps1
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if (-not (Test-Path -Path "venv")) {
+    python -m venv venv
+    .\venv\Scripts\Activate.ps1
+    pip install -r requirements.txt 
+} else {
+    .\venv\Scripts\Activate.ps1
+}
+
+python -m src.game.TestReflexesGame
